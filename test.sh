2cea0506f20b6d01408c64b970d44aca71fd3e03
Riccardo Cecchini <rcecchini.ds@gmail.com>
2025-03-07 13:01:28 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-#python3 main.py --mode compress --input chopin.wav --output chopin.sin 
-python3 main.py --mode decompress --input chopin.sin --output chopin.back.wav
+python3 main.py --mode compress --input chopin.wav --output chopin.sin 
+#python3 main.py --mode decompress --input chopin.sin --output chopin.back.wav
